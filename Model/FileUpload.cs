7990c89e9edacf3d81a6dbe8dc57521e19f1d5fd
fslx <jorgenmjos@hotmail.com>
2025-12-29 13:35:36 +0100

    added the first controller

--- Model/FileUpload.cs
+++ Model/FileUpload.cs
@@ -3,5 +3,9 @@ public class FileUpload
     public int Id { get; set; }
     public string FileName { get; set; } = string.Empty;
     public long FileSize { get; set; }
+    /// <summary>
+    /// Automatically set this server-side
+    /// </summary>
+    public Guid Guid { get; set; } = Guid.NewGuid();
 
 }
